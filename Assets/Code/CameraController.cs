36f9c182a48a0cea5832b647d7f44d1e119f8b74
linda4321 <magur.polina@gmail.com>
2018-05-19 18:40:19 +0300

    First homework

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public HeroRabit rabit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform rabitTransform = rabit.transform;
+		Transform cameraTransform = this.transform;
+
+		Vector3 rabitPos = rabitTransform.position;
+		Vector3 cameraPos = cameraTransform.position;
+
+		cameraPos.x = rabitPos.x;
+		cameraPos.y = rabitPos.y;
+
+		cameraTransform.position = cameraPos;
+	}
+}
---
+++ Assets/Code/HeroRabit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabit : MonoBehaviour {
+
+	public float speed = 1;
+
+	private Rigidbody2D rabitBody = null;
+	private SpriteRenderer rabitSprite;
+
+	// Use this for initialization
+	void Start () {
+		rabitBody = this.GetComponent<Rigidbody2D> ();
+		rabitSprite = this.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		float value = Input.GetAxis ("Horizontal");
+
+		Vector2 velocity = rabitBody.velocity;
+		velocity.x = speed * value;
+		rabitBody.velocity = velocity;
+
+		if (value > 0) {
+			rabitSprite.flipX = false;
+		} else if (value < 0) {
+			rabitSprite.flipX = true;
+		}
+	}
+}
